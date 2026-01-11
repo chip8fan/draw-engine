6afaf4f2613895d26368d1df82163567fcddb4b3
Nguyen Tran <me@mac.local>
2026-01-10 18:41:50 -0600

    streamline analysis of each root node

--- draw-engine.py
+++ draw-engine.py
@@ -21,28 +21,11 @@ while True:
             for move in move_list:
                 board.push_uci(move)
     elif line[0] == "go":
-        if "wtime" in line:
-            white_clock = int(line[line.index("wtime")+1])/1000
-        else:
-            white_clock = None
-        if "btime" in line:
-            black_clock = int(line[line.index("btime")+1])/1000
-        else:
-            black_clock = None
-        if "winc" in line:
-            white_inc = int(line[line.index("winc")+1])/1000
-        else:
-            white_inc = None
-        if "binc" in line:
-            black_inc = int(line[line.index("binc")+1])/1000
-        else:
-            black_inc = None
-        num = len(list(board.legal_moves))
         multipv = 3
         moves = []
         for move in board.legal_moves:
             board.push(move)
-            moves.append([abs(engine.analyse(board, chess.engine.Limit(white_clock=white_clock/num, black_clock=black_clock/num, white_inc=white_inc, black_inc=black_inc))["score"].relative.score(mate_score=sys.maxsize)), move])
+            moves.append([abs(engine.analyse(board, chess.engine.Limit(time=1))["score"].relative.score(mate_score=sys.maxsize)), move])
             board.pop()
         print(f"bestmove {random.choice(sorted(moves, key=lambda x: x[0])[:multipv])[1]}")
     elif line[0] == "quit":
